cca5a33ce228e46b624e6bf42e8a3b38dc6f4e54
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-09-14 17:23:03 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
-import { Layout, Typograpgy, Space } from 'antd'
+import { Layout, Typography, Space } from 'antd'
 
 import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
 import './App.css'
@@ -33,9 +33,17 @@ const App = () => {
                     </Routes>
                 </div>
             </Layout>
-        </div>
-        <div className='footer'>
-
+            <div className='footer'>
+                <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                    Crypotona <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
+            </div>
         </div>
     </div>
   )
